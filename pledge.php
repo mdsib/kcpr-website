74ea7e8d4eb851c2d19d719f53cd9eb08b5a5abe
mdsib <mdsiboldi@gmail.com>
2013-05-12 14:19:36 +0000

    update

---
+++ pledge.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/pledge.css" />
+<script type="text/javascript" src="js/jQuery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/goal.js"></script>
+</head>
+<div id="content">
+<div id="thermometer">
+   <div class="track">
+       <div class="goal">
+          <div class="amount">5000</div>
+      </div>
+      <div class="progress">
+          <div class="amount">0</div>
+      </div>
+   </div>
+</div>
+</div>
+</html>
