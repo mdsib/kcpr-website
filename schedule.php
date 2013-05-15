9b29319cfe94db7f247dc6f48d3dd6ee8ac0f386
mdsib <mdsiboldi@gmail.com>
2013-05-15 23:20:42 +0000

    ajax work

--- schedule.php
+++ schedule.php
@@ -641,7 +641,8 @@
      //days->day->show->info
      //$days [0-6]  [0-?]   [0,1,2]
   
-        echo '<div id="ajax_content" class="border-module content" style="height:1200px">';
+        echo '<div id="ajax_content" class="border-module" style="height:1200px">';
+        echo '<div id="content">';
         print_key();
         print_calendar_time();
         for ($d=0; $d<7; $d++)
@@ -653,6 +654,7 @@
            }
            echo "</div>";
         }
-        echo "</div>";
+        echo "</div>
+        </div>";
    }
 ?>
