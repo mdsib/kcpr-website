be3eb5a7e44741ff07e1dfbba59288419ec7b891
Eric Buckthal <ebuckthal@gmail.com>
2013-05-15 13:14:59 -0700

    donations?

--- php/header.php
+++ php/header.php
@@ -2,28 +2,28 @@
 function print_header($title)
 {
 ?>
-<html>
+   <html>
    <head>
-      <!-- web browsers like css here instead of in php files -->
-      <link href='http://fonts.googleapis.com/css?family=Lato' 
-         rel='stylesheet' 
-         type='text/css'>
-      <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:300'
-         rel='stylesheet'
-         type='text/css'>
-      <link rel="stylesheet" type="text/css" href="css/main.css">
-      <link rel="stylesheet" type="text/css" href="css/schedule.css">
-      <!-- web browsers like css here instead of in php files -->
-      
-      <audio preload="false" id="streamer" name="media">
-         <source src="http://129.65.35.106:8000/KCPRHIGH" type="audio/aac">
-         <source src="http://129.65.35.106:8000/KCPRMP3" type="audio/mpeg">
-      </audio>
+   <!-- web browsers like css here instead of in php files -->
+   <link href='http://fonts.googleapis.com/css?family=Lato'
+   rel='stylesheet'
+   type='text/css'>
+   <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:300'
+   rel='stylesheet'
+   type='text/css'>
+   <link rel="stylesheet" type="text/css" href="css/main.css">
+   <link rel="stylesheet" type="text/css" href="css/schedule.css">
+   <!-- web browsers like css here instead of in php files -->
 
-      <link rel="shortcut icon" href='favicon.ico'/>
-      <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-      <script type="text/javascript" src="js/ajax-version-1.0.js"></script>
-      <script type="text/javascript" src="http://4nf.org/history.min.js"></script>
+   <audio preload="false" id="streamer" name="media">
+   <source src="http://129.65.35.106:8000/KCPRHIGH" type="audio/aac">
+   <source src="http://129.65.35.106:8000/KCPRMP3" type="audio/mpeg">
+   </audio>
+
+   <link rel="shortcut icon" href='favicon.ico'/>
+   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+   <script type="text/javascript" src="js/ajax-version-1.0.js"></script>
+   <script type="text/javascript" src="http://4nf.org/history.min.js"></script>
 
    <script type="text/javascript" src="js/fixMenuToTop.js"></script>
 
--- pledge.php
+++ pledge.php
@@ -17,12 +17,12 @@ function print_content() {
    <table>
    <tr><td><input type="hidden" name="on0" value="Value">Donate</td></tr><tr><td>
    <select name="os0">
-      <option value="Option 1">$5.00</option>
-      <option value="Option 2">$10.00</option>
-      <option value="Option 3">$25.00</option>
-      <option value="Option 4">$50.00</option>
-      <option value="Option 5">$100.00</option>
-      <option value="Option 6">$250.00</option>
+   <option value="Option 1">$5.00</option>
+   <option value="Option 2">$10.00</option>
+   <option value="Option 3">$25.00</option>
+   <option value="Option 4">$50.00</option>
+   <option value="Option 5">$100.00</option>
+   <option value="Option 6">$250.00</option>
    </select> </td></tr>
    </table>
    <input type="hidden" name="currency_code" value="USD">
@@ -39,15 +39,18 @@ function print_content() {
    <input type="hidden" name="option_select5" value="Option 6">
    <input type="hidden" name="option_amount5" value="250.00">
    <input type="hidden" name="option_index" value="0">
-   <input type="image" 
-      src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" 
-      border="0" 
-      name="submit" 
-      alt="PayPal - The safer, easier way to pay online!">
-   <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-   </form>
+   <input type="image"
+   src="https://www.paypalobjects.com/webstatic/mktg/logo/AM_mc_vs_dc_ae.jpg"
+   border="0"
+   name="submit"
+   alt="PayPal - The safer, easier way to pay online!">
+   <!--img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"-->
+   <img alt="" border="0" src="https://www.paypalobjects.com/webstatic/mktg/logo/AM_mc_vs_dc_ae.jpg" width="1" height="1">
    
+   </form>
+
    </div>
 <?php
 }
 ?>
+   <!--src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif"-->
