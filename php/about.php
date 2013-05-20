972485cc1b0f28f8dfb7894cd6328ddfbda6c3db
mdsib <mdsiboldi@gmail.com>
2013-05-20 00:01:16 +0000

    pledge mark;

--- php/about.php
+++ php/about.php
@@ -1,22 +1,35 @@
 <div id="about" class="content">
+<img width=100% src="images/kcpr_squad.jpg">
+<!--img width=100% src="images/beach_crew.jpg"-->
+
+<h1>Who We Are</h1>
 <p>
    KCPR is a stereo FM radio station broadcasting throughout the Central
-        Coast of California on 91.3 MHz. It is the college station of Cal Poly,
-            in San Luis Obispo. We broadcast 24/7 online and on air. We play
-            music along with a few news and talk shows.
+   Coast of California on 91.3 MHz. It is the college station of Cal Poly,
+   in San Luis Obispo. 
 </p>
 <p>
    The station is run by volunteer college students and community members.
    We all do it from our love of music, our community, and the radio.
 </p>
 <p>
-   We don't get much money from the university, so we ask community members
-   and listeners to donate. We'd like it if you did so we can keep broastcasting.
-   Every year we have a Pledge Drive, where you can bid on packages of neat items.
-   The money we make goes towards buying new equipment and music.
-         </p>
-         <p>
-            We have been broadcasting since 1968.
-         </p>
-         <h1>What we do</h1>
-         </div>
+   We have been broadcasting since 1968.
+</p>
+
+<h1>What We Do</h1>
+<p>
+   We play mostly music, and have some talk and news shows. There are themed
+   shows that are genre or theme specific,
+   and more general "regular shows" that cover new and old music added to the
+   station. We write and produce announcements, 
+   We broadcast 24/7 online and on air. 
+</p>
+
+<h1>What You Can Do</h1>
+<p>
+   We are a volunteer station at a public college, providing ad free radio to
+   the Central Coast. In other words, we <i>need</i> your support to keep doing it!
+   We'd appreciate it if you write us a nice letter or check. Or donate online! Or
+   even just listen, we'd like that.
+</p>
+</div>
--- php/pledge.php
+++ php/pledge.php
@@ -14,14 +14,14 @@ function print_thermometer()
 </div>
 <div id="pledge" class="content">
    <div id="pledge_video">
-   <iframe width="700" height="420" src="http://www.youtube.com/embed/wDCLgTti7xk" frameborder="0" allowfullscreen></iframe>
-   <p> This week, we are asking our loving community members and listeners to help us keep our station running smoothly. The facts are: We recieve x dollars from blah. We are planning on buying new equipment for remote broadcasting to up our quality and diversity of coverage. You could hear live shows, interviews, and much more from KCPR! We're also looking to upgrade a few parts in our station and perform general maintenence work. We would love your support. Really!<br><br>
+   <iframe width="700" height="420" src="http://www.youtube.com/embed/ywNXRsW-17Q" frameborder="0" allowfullscreen></iframe>
+   <p> This week, we are asking our loving community members and listeners to help us keep our station running smoothly. We are planning on buying new equipment for remote broadcasting to up our quality and diversity of coverage. You could hear live shows, interviews, and much more from KCPR! We're also looking to upgrade a few parts in our station and perform general maintenence work. We would love your support. Really!<br><br>
 
    To show you how much we care, we'll give you some cool stuff in return.<br><br>
 
    </div>
 
-   <img class="pledge_gift" src="/images/pledge/kcpr_mug.jpg" alt="KCPR mug"></img>
+   <!--img class="pledge_gift" src="/images/pledge/kcpr_mug.jpg" alt="KCPR mug"></img-->
 
    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
    <input type="hidden" name="cmd" value="_xclick">
@@ -33,39 +33,39 @@ function print_thermometer()
    <input type="hidden" name="button_subtype" value="services">
    <input type="hidden" name="no_note" value="0">
    <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-   <button class="donate_button" type="submit" name="submit">DONATE $5</button><br>
-   Giving just for the sake of giving. Thanks for your support, we really appreciate it!
+   <center><button class="donate_button" type="submit" name="submit">DONATE $5</button><br></center>
+   Giving just for the sake of giving. Thanks for your support, we really appreciate it! Really!
 
    <input type="hidden" name="amount" value="10.00">
-   <button class="donate_button" type="submit" name="submit">DONATE $10</button><br>
+   <center><button class="donate_button" type="submit" name="submit">DONATE $10</button><br></center>
    Get KCPR-style items: A sturdy tote bag for groceries, shark shaped bottle opener, and a magnet to put somewhere special.<br>
 
    <input type="hidden" name="amount" value="15.00">
    <input type="hidden" name="item_name" value="KCPR Donation 15.00">
-   <button class="donate_button" type="submit" name="submit">DONATE $15</button><br>
+   <center><button class="donate_button" type="submit" name="submit">DONATE $15</button><br></center>
    Get everything from $10, plus KCPR apparel brand sunglasses to look rad on a hot day.<br>
    <input type="hidden" name="amount" value="25.00">
    <input type="hidden" name="item_name" value="KCPR Donation 25.00">
-   <button class="donate_button" type="submit" name="submit">DONATE $25</button><br>
+   <center><button class="donate_button" type="submit" name="submit">DONATE $25</button><br></center>
    Get KCPR stuff: Everything from $15, plus a sweet KCPR mug for loose leaf tea or Chemex coffee.<br>
 
    <input type="hidden" name="amount" value="50.00">
    <input type="hidden" name="item_name" value="KCPR Donation 50.00">
-   <button class="donate_button" type="submit" name="submit">DONATE $50</button><br>
+   <center><button class="donate_button" type="submit" name="submit">DONATE $50</button><br></center>
    On top of everything above, you get a DJ love note, written personally and with love just for you. Hang it on your wall, frame it, keep it safe and close to your heart.<br>
 
    <input type="hidden" name="amount" value="100.00">
    <input type="hidden" name="item_name" value="KCPR Donation 100.00">
-   <button class="donate_button" type="submit" name="submit">DONATE $100</button><br>
+   <center><button class="donate_button" type="submit" name="submit">DONATE $100</button></center>
    Get KCPR'd out with every KCPR item, a love note, and... A personalized mix CD! Give us three adjectives and your name and we'll come up with something that may shake your booty off. It's a mystery CD for an awesome supporter.<br>
 
    <input type="hidden" name="amount" value="250.00">
    <input type="hidden" name="item_name" value="KCPR Donation 250.00">
-   <button class="donate_button" type="submit" name="submit">DONATE $250</button>
+   <center><button class="donate_button" type="submit" name="submit">DONATE $250</button>
    <input type="hidden" name="amount" value="500.00">
    <input type="hidden" name="item_name" value="KCPR Donation 500.00">
-   <button class="donate_button" type="submit" name="submit">or $500</button><br>
-   <big><i>OMG!</i></big> We literally <big>LOVE</big> you!<br> To prove it, you can get everything from previous rewards, a personally dyed t-shirt, a hug from your favorite DJ, and a guest spot on their show! A lifechanging experience. Only donate this much if you are ready to change your life in a positive way.
+   <button class="donate_button" type="submit" name="submit">or $500</button></center>
+   <big><i>OMG!</i></big> We literally <big>LOVE</big> you! To prove it, you can get everything from previous rewards, a personally dyed t-shirt, a hug from your favorite DJ, and a guest spot on their show! A lifechanging experience. Only donate this much if you are ready to change your life in a positive way.
 
    </form>
    </div>
