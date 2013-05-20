c3fe48d4d710baf81c2455fcce0b69b9313a7f16
Eric Buckthal <ebuckthal@gmail.com>
2013-05-19 19:40:39 -0700

    maybe

--- php/news.php
+++ php/news.php
@@ -8,7 +8,7 @@ $blog = simplexml_load_file("../xml/blog.xml", 'SimpleXMLIterator');
 foreach($blog as $post) {
    echo "<div class='post'>";
       echo "<div class='post_heading'>";
-         echo "<date>".$post->date." at ".$post->time."</date>";
+         echo "<date>".$post->date; 
          echo "<post_title>".$post->title."</post_title>";
       echo "</div>";
       echo "<a target='_blank' href='".$post->image."'><img src='".$post->image."'></img></a>";
