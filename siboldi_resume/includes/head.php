456508d936756e715c7a722f3eb274bffbaa16a5
mdsib <mdsiboldi@gmail.com>
2013-06-24 11:30:56 +0000

    deleted unnecessary files

--- siboldi_resume/includes/head.php
+++
@@ -1,6 +0,0 @@
-<header>
-   <link href='http://fonts.googleapis.com/css?family=Istok+Web:400,400italic' rel='stylesheet' type='text/css'>
-   <link rel="stylesheet" type="text/css" href="css/style.css" />
-   <link rel="icon" href="images/siboldi.ico" />
-   <h1>Massimo Siboldi</h1>
-</header>
--- siboldi_resume/resume.php
+++
@@ -1,7 +0,0 @@
-<?php include("includes/head.php"); ?>
-<h2>Education</h2>
-<h3>Cal Poly, SLO (2010 - present)</h3>
-<h2>Experience</h2>
-<h2>Skills</h2>
-
-
